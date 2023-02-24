80e68f14557792cb0bc74d9d93991635d7bbe867
خایه مال علی سورنا <mr.nima.c@gmail.com>
2023-02-25 02:16:35 +0330

    add personal section view for add and list

--- AdminPanel/views.py
+++ AdminPanel/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404, redirect
+from CustomUser.models import UserProfile
+def dashboard(request):
+    context = {} #TODO: ask project manager for date needs to be use in panel
+    return render(request, 'index.html', context)
 
-# Create your views here.
+#* PERSONEL BLOCK
+def personel_list(request):
+    context = {
+        'personels' : UserProfile.objects.filter(is_active=True,is_staff=True).order_by('date_joined'),
+    }
+    return render(request, 'listkarmand.html', context)
+def personel_add(request):
+    personel_form = PersonelForm(request.POST or None)
+
+    if personel_form.is_valid():
+        personel_form.save()
+        return redirect() #TODO: redirect to personal list
+
+
