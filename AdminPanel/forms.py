847009cd5185aee9eb0bc285a4a6843023d5c783
خایه مال علی سورنا <mr.nima.c@gmail.com>
2023-02-25 02:17:59 +0330

    add form for add & update personel view

---
+++ AdminPanel/forms.py
@@ -0,0 +1,30 @@
+from CustomUser.models import UserProfile
+from django import forms
+
+class PersonelForm(forms.ModelForm):
+
+    password1 = forms.CharField(
+        label=("Password"),
+        strip=False,
+        widget=forms.PasswordInput(attrs={"autocomplete": "new-password"}),
+        # help_text=password_validation.password_validators_help_text_html(),
+    )
+
+    class Meta:
+        model = UserProfile
+        fields = ('username', 'name', 'email', 'phone_number', 'address', 'postal_code', 'city',)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if self._meta.model.USERNAME_FIELD in self.fields:
+            self.fields[self._meta.model.USERNAME_FIELD].widget.attrs[
+                "autofocus"
+            ] = True
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        user.is_staff = True
+        if commit:
+            user.save()
+        return user
